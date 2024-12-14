7c38536f35a602e666f79fd929fb37066a8f19a0
Nazeem Kamel <nazeem.kamel@gmail.com>
2024-12-13 23:40:45 -0800

    Update project configuration, CameraViewController, and add images

--- Sumo/CameraViewController.swift
+++ Sumo/CameraViewController.swift
@@ -1,22 +1,31 @@
 import UIKit
 import AVFoundation
 
+extension Array {
+    subscript(safe index: Int) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
+
 class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureDepthDataOutputDelegate {
     private var captureSession: AVCaptureSession!
     private var previewLayer: AVCaptureVideoPreviewLayer!
     private let frameProcessingInterval: TimeInterval = 2.0
     private var lastFrameTime = Date.distantPast
     private var lastDepthData: [Float]?
-
-    private let detectionLabel: UILabel = {
-        let label = UILabel()
-        label.text = ""
-        label.textColor = .red
-        label.backgroundColor = .black.withAlphaComponent(0.5)
-        label.font = UIFont.boldSystemFont(ofSize: 20)
-        label.textAlignment = .center
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+    private var shapeLayer: CAShapeLayer!
+    
+    private let detectionTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = ""
+        textView.textColor = .red
+        textView.backgroundColor = .black.withAlphaComponent(0.5)
+        textView.font = UIFont.boldSystemFont(ofSize: 20)
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.isScrollEnabled = true
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
     }()
 
     override func viewDidLoad() {
@@ -25,19 +34,27 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         title = "Camera"
 
         setupCamera()
-        setupDetectionLabel()
+        setupShapeLayer()
+        setupdetectionTextView()
     }
-
-    private func setupDetectionLabel() {
-        view.addSubview(detectionLabel)
+    
+    private func setupdetectionTextView() {
+        view.addSubview(detectionTextView)
 
         NSLayoutConstraint.activate([
-            detectionLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
-            detectionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            detectionLabel.widthAnchor.constraint(equalToConstant: 200),
-            detectionLabel.heightAnchor.constraint(equalToConstant: 40)
-
+            detectionTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            detectionTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            detectionTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            detectionTextView.heightAnchor.constraint(equalToConstant: 100)
         ])
+    }
+
+    private func setupShapeLayer() {
+        shapeLayer = CAShapeLayer()
+        shapeLayer.frame = view.bounds
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.fillColor = UIColor.red.cgColor
+        view.layer.addSublayer(shapeLayer)
     }
 
     private func setupCamera() {
@@ -59,11 +76,12 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
             captureSession.addOutput(videoOutput)
         }
 
-        let depthDataOutput = AVCaptureDepthDataOutput()
-        depthDataOutput.setDelegate(self, callbackQueue: DispatchQueue(label: "depthDataQueue"))
-        if captureSession.canAddOutput(depthDataOutput) {
-            captureSession.addOutput(depthDataOutput)
-        }
+        // Enable to capture LIDAR depth data
+//        let depthDataOutput = AVCaptureDepthDataOutput()
+//        depthDataOutput.setDelegate(self, callbackQueue: DispatchQueue(label: "depthDataQueue"))
+//        if captureSession.canAddOutput(depthDataOutput) {
+//            captureSession.addOutput(depthDataOutput)
+//        }
 
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.frame = view.layer.bounds
@@ -71,7 +89,7 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         view.layer.addSublayer(previewLayer)
 
         // Ensure the constant label is always on top
-        view.bringSubviewToFront(detectionLabel)
+        view.bringSubviewToFront(detectionTextView)
 
         DispatchQueue.global(qos: .userInitiated).async {
             self.captureSession.startRunning()
@@ -85,7 +103,23 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
 
         guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         let ciImage = CIImage(cvPixelBuffer: pixelBuffer)
-        let uiImage = UIImage(ciImage: ciImage)
+ 
+        // Adjust the image orientation based on the device orientation
+        let orientation: UIImage.Orientation
+        switch UIDevice.current.orientation {
+        case .portrait:
+            orientation = .right
+        case .portraitUpsideDown:
+            orientation = .left
+        case .landscapeLeft:
+            orientation = .up
+        case .landscapeRight:
+            orientation = .down
+        default:
+            orientation = .right
+        }
+
+        let uiImage = UIImage(ciImage: ciImage, scale: 1.0, orientation: orientation)
 
         sendFrameToBackend(image: uiImage, depthData: lastDepthData)
     }
@@ -109,20 +143,80 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         lastDepthData = depthArray
     }
 
-    struct SkeletonPoint: Codable {
-        let x: Float
-        let y: Float
-        let z: Float
-        let visibility: Float
+    private func drawSkeleton(_ skeleton: [SkeletonPoint]) {
+        shapeLayer.sublayers?.forEach { $0.removeFromSuperlayer() }
+
+        guard !skeleton.isEmpty else {
+            return
+        }
+      
+        // Define the connections between the skeleton points
+        let connections: [(Int, Int)] = [
+            (0,1),(1,2),(2,3),(3,7),
+            (0,4),(4,5),(5,6),(6,8),
+            (9,10),
+            (11,12),(11,13),(12,14),
+            (13,15),(14,16),
+            (15,17),(17,19),(19,15),(15,21),
+            (16,18),(18,20),(20,16),(16,21),
+            (11,23),(12,24),(23,24),
+            (23,25),(25,27),(27,29),(29,31),(31,27),
+            (24,26),(26,28),(28,30),(30,32),(32,28)
+        ]
+
+        // Draw the connections
+        for connection in connections {
+            if let startPoint = skeleton[safe: connection.0], let endPoint = skeleton[safe: connection.1] {
+                let startPoint = CGPoint(x: CGFloat(startPoint.x) * view.bounds.width, y: CGFloat(startPoint.y) * view.bounds.height)
+                let endPoint = CGPoint(x: CGFloat(endPoint.x) * view.bounds.width, y: CGFloat(endPoint.y) * view.bounds.height)
+                let linePath = UIBezierPath()
+                linePath.move(to: startPoint)
+                linePath.addLine(to: endPoint)
+                let lineLayer = CAShapeLayer()
+                lineLayer.path = linePath.cgPath
+                lineLayer.strokeColor = UIColor.green.cgColor
+                lineLayer.lineWidth = 2.0
+                shapeLayer.addSublayer(lineLayer)
+            }
+        }
+
+        // Draw the min heigh line
+        let yMin = skeleton.map { $0.min }.min() ?? 0
+        let minHeighLinePath = UIBezierPath()
+        minHeighLinePath.move(to: CGPoint(x: 0, y: CGFloat(yMin) * view.bounds.height))
+        minHeighLinePath.addLine(to: CGPoint(x: view.bounds.width, y: CGFloat(yMin) * view.bounds.height))
+        let minHeighLineLayer = CAShapeLayer()
+        minHeighLineLayer.path = minHeighLinePath.cgPath
+        minHeighLineLayer.strokeColor = UIColor.red.cgColor
+        minHeighLineLayer.lineWidth = 1.0
+        minHeighLineLayer.lineDashPattern = [4, 4]
+        shapeLayer.addSublayer(minHeighLineLayer)
+
+        // Draw the points
+        for point in skeleton {
+            let circlePath = UIBezierPath(arcCenter: CGPoint(x: CGFloat(point.x) * view.bounds.width, y: CGFloat(point.y) * view.bounds.height), radius: 5, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+            let circleLayer = CAShapeLayer()
+            circleLayer.path = circlePath.cgPath
+            circleLayer.fillColor = point.ground_contact ? UIColor.red.cgColor : UIColor.blue.cgColor
+            shapeLayer.addSublayer(circleLayer)
+        }
+
     }
 
+    struct SkeletonPoint: Codable {
+        let x: Float    
+        let y: Float
+        let min: Float
+        let label: String
+        let ground_contact: Bool
+    }
+    
     struct ProcessFrameResponse: Codable {
         let skeleton: [SkeletonPoint]
-        let ground_contacts: [String]
     }
 
     private func sendFrameToBackend(image: UIImage, depthData: [Float]?) {
-        guard let url = URL(string: "http://10.0.0.22:8000/process-frame/") else { return }
+        guard let url = URL(string: "http://10.15.162.38:8000/process-frame/") else { return }
 
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
@@ -134,7 +228,7 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
 
         var body = Data()
         body.append("--\(boundary)\r\n".data(using: .utf8)!)
-        body.append("Content-Disposition: form-data; name=\"file\"; filename=\"frame.jpg\"\r\n".data(using: .utf8)!)
+        body.append("Content-Disposition: form-data; name=\"frame\"; filename=\"frame.jpg\"\r\n".data(using: .utf8)!)
         body.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
         body.append(imageData)
         body.append("\r\n".data(using: .utf8)!)
@@ -142,7 +236,7 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         if let depthData = depthData {
             let depthDataString = depthData.map { String($0) }.joined(separator: ",")
             body.append("--\(boundary)\r\n".data(using: .utf8)!)
-            body.append("Content-Disposition: form-data; name=\"depthData\"; filename=\"depth.txt\"\r\n".data(using: .utf8)!)
+            body.append("Content-Disposition: form-data; name=\"depth\"; filename=\"depth.txt\"\r\n".data(using: .utf8)!)
             body.append("Content-Type: text/plain\r\n\r\n".data(using: .utf8)!)
             body.append(depthDataString.data(using: .utf8)!)
             body.append("\r\n".data(using: .utf8)!)
@@ -157,7 +251,17 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
                 return
             }
 
-            guard let data = data else {
+            guard let httpResponse = response as? HTTPURLResponse else {
+                print("Error: No HTTP response received.")
+                return
+            }
+
+            guard (200...299).contains(httpResponse.statusCode) else {
+                print("Error: HTTP response code \(httpResponse.statusCode)")
+                return
+            }
+
+            guard let data = data, !data.isEmpty else {
                 print("Error: No data received.")
                 return
             }
@@ -165,9 +269,11 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
             do {
                 let result = try JSONDecoder().decode(ProcessFrameResponse.self, from: data)
                 DispatchQueue.main.async {
-                    let groundContacts = result.ground_contacts
-                    let label = groundContacts.isEmpty ? "No contacts" : groundContacts.joined(separator: ", ")
-                    self.detectionLabel.text = " contacts : \(label)"
+                    let groundContacts = result.skeleton.filter { $0.ground_contact }
+                    let groundContactsLabels = groundContacts.map { $0.label }
+                    let label = groundContacts.isEmpty ? "No contacts" : groundContactsLabels.joined(separator: ", ")
+                    self.detectionTextView.text = label
+                    self.drawSkeleton(result.skeleton)
                     print("Frame captured at \(label)")
                 }
             } catch {
--- main.py
+++ main.py
@@ -16,17 +16,33 @@ pose = mp_pose.Pose(static_image_mode=True, model_complexity=1, min_detection_co
 GROUND_THRESHOLD = 0.2  # Adjust this value based on your setup
 
 @app.post("/process-frame/")
-async def process_frame(file: UploadFile = File(...)):
+async def process_frame(frame: UploadFile = File(...)):
     """
-    Endpoint to process a single image frame for pose estimation.
+    Endpoint to process a single image frame and depth data for pose estimation.
     """
-    if not file.filename.endswith(("jpg", "jpeg", "png")):
-        raise HTTPException(status_code=400, detail="Invalid file type. Please upload an image.")
+    if not frame.filename.endswith(("jpg", "jpeg", "png")):
+        raise HTTPException(status_code=400, detail="Invalid file type for frame. Please upload an image.")
+    # if depth and not depth.filename.endswith("txt"):
+    #     raise HTTPException(status_code=400, detail="Invalid file type for depth data. Please upload a text file.")
 
     try:
         # Read and decode the image
-        file_data = await file.read()
-        image = cv2.imdecode(np.frombuffer(file_data, np.uint8), cv2.IMREAD_COLOR)
+        frame_data = await frame.read()
+        image = cv2.imdecode(np.frombuffer(frame_data, np.uint8), cv2.IMREAD_COLOR)
+
+        # Read and parse the depth data if provided
+        # depth_array = None
+        # if depth:
+        #     depth_data = await depth.read()
+        #     depth_array = np.array([float(x) for x in depth_data.decode().split(",")])
+
+        #     # for debugging purposes, store the depth data
+        #     with open("depth.txt", "wb") as file_d:
+        #         file_d.write(depth_data)
+
+        # # for debugging purposes, store the image
+        # with open("frame.jpg", "wb") as file_f:
+        #     file_f.write(frame_data)
 
         # Convert image to RGB for MediaPipe
         rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -45,11 +61,13 @@ async def process_frame(file: UploadFile = File(...)):
                     "visibility": landmark.visibility  # Confidence
                 })
 
-        # Check for ground contact
-        ground_contacts = detect_ground_contact(skeleton_points)
+        # TODO: Integrate depth data with skeleton points
 
-        print("Result:", {"skeleton": skeleton_points, "ground_contacts": ground_contacts})
-        return {"skeleton": skeleton_points, "ground_contacts": ground_contacts}
+        # Check for ground contact
+        results = detect_ground_contact(skeleton_points)
+
+        print("Result:", {"skeleton": results})
+        return {"skeleton": results}
 
     except Exception as e:
         print("Error processing frame:", e)
@@ -59,7 +77,7 @@ def detect_ground_contact(skeleton_points: List[Dict[str, float]]) -> List[str]:
     """
     Detects if any joint other than the feet is close to the ground.
     """
-    ground_contacts = []
+    results = []
     joint_names = [
         "nose", "left_eye_inner", "left_eye", "left_eye_outer", "right_eye_inner", "right_eye",
         "right_eye_outer", "left_ear", "right_ear", "mouth_left", "mouth_right", "left_shoulder",
@@ -70,14 +88,18 @@ def detect_ground_contact(skeleton_points: List[Dict[str, float]]) -> List[str]:
     ]
 
     # Indices for foot joints to exclude from detection
-    foot_indices = {27, 28, 31, 32}  # left_ankle, right_ankle, left_foot_index, right_foot_index
+    foot_indices = {27, 28, 29, 30, 31, 32}  # "left_ankle", "right_ankle", "left_heel", "right_heel", "left_foot_index", "right_foot_index"
 
+    if len(skeleton_points) != 33:
+        return results
+    min_foot_y = min(skeleton_points[27]["y"],skeleton_points[28]["y"],skeleton_points[29]["y"],skeleton_points[30]["y"],skeleton_points[31]["y"],skeleton_points[32]["y"])
     for i, point in enumerate(skeleton_points):
-        # if i not in foot_indices and point["visibility"] > 0.5 and 
-        if point["z"] < GROUND_THRESHOLD:
-            ground_contacts.append(joint_names[i])
+       if i not in foot_indices and point["visibility"] > 0.5 and point["y"] >= min_foot_y:
+           results.append({"x": point["x"], "y": point["y"], "min": min_foot_y, "label": joint_names[i], "ground_contact": True})
+       else:
+          results.append({"x": point["x"], "y": point["y"], "min": min_foot_y, "label": joint_names[i], "ground_contact": False})
 
-    return ground_contacts
+    return results
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
